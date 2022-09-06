352d583662e9de13c630d4405f987e1488c23223
Thaua <thauanlucascpl@gmail.com>
2022-09-05 21:49:32 -0300

    bug correction

--- src/components/CalculatorButton/calculatorButton.js
+++ src/components/CalculatorButton/calculatorButton.js
@@ -1,10 +1,15 @@
 import "./calculatorButton.css";
 
 export default function CalculatorButton(props) {
-  const re = /(^[+=/*]|([+=/*]{2}|([-+][+=/*-])|[*][=]$))/;
+  const re = /(^[+=/*]|([+=/*]{2}|([-+][+=/*-]|[=][*]])$))/;
+  const re2= /([+=/*-]{3})/ 
   const handleClick = () => {
     if (re.test(props.expressao + props.value)) {
-      props.setExpressao(props.expressao.substr(0, props.expressao.length - 1));
+      if (re2.test(props.expressao + props.value)){
+        props.setExpressao(props.expressao.substr(0, props.expressao.length - 2));        
+      } else {
+        props.setExpressao(props.expressao.substr(0, props.expressao.length - 1));
+      }
     }
     props.setExpressao((preValue) => preValue + props.value);
   };
